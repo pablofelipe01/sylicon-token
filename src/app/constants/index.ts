0de55c9529323afb5808a0f04d3add767932004e
Pablo Acebedo <pablofelipe@me.com>
2024-07-12 13:57:51 -0500

    New Icons and logo

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,41 +2,51 @@ export const inclusions = [
   {
     title: 'Garantia Fiduciaria',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan.',
-    icon: '/assets/icons/shipping.svg',
+    icon: '/assets/icons/icono2.svg',
   },
   {
-    title: 'Money Guarantee',
-    description: 'Within 30 days for an exchange',
+    title: 'Pagos trimestrales',
+    description: 'Directo a tu billetera',
     icon: '/assets/icons/dollar.svg',
   },
   {
-    title: 'Soporte en Linea',
+    title: 'Soporte',
     description: '24/7',
     icon: '/assets/icons/support.svg',
   },
   {
     title: 'Pagos Flexibles',
-    description: 'Pay with multiple credit cards',
+    description: 'Con cualquier medio de pago',
     icon: '/assets/icons/payment.svg',
+  },
+  {
+    title: 'Respaldo',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan',
+    icon: '/assets/icons/shipping.svg',
+  },
+  {
+    title: 'Soporte',
+    description: 'info@sylicon.com.co',
+    icon: '/assets/icons/icono3.svg',
   },
 ]
 
 export const profileNavItems = [
   {
-    title: 'Personal Information',
+    title: 'Información Personal',
     url: '/account',
     icon: '/assets/icons/user.svg',
   },
-  {
-    title: 'My Purchases',
-    url: '/account/purchases',
-    icon: '/assets/icons/purchases.svg',
-  },
-  {
-    title: 'My Orders',
-    url: '/account/orders',
-    icon: '/assets/icons/orders.svg',
-  },
+  // {
+  //   title: 'My Purchases',
+  //   url: '/account/purchases',
+  //   icon: '/assets/icons/purchases.svg',
+  // },
+  // {
+  //   title: 'My Orders',
+  //   url: '/account/orders',
+  //   icon: '/assets/icons/orders.svg',
+  // },
   {
     title: 'Logout',
     url: '/logout',
